20f6fdb0188b3dceea5ac5cff80dcbf2eafd510d
Phil Zhang <371418433@qq.com>
2020-06-26 16:55:34 +0800

    add No21MergeTwoSortedLists.java

---
+++ Week_01/No21MergeTwoSortedLists.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    /**
+     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+     ListNode preHead = new ListNode(-1);
+     ListNode preNode = preHead;
+     while (l1 != null && l2 != null) {
+     if (l1.val < l2.val) {
+     preNode.next = l1;
+     l1 = l1.next;
+     } else {
+     preNode.next = l2;
+     l2 = l2.next;
+     }
+     preNode = preNode.next;
+     }
+
+     if (l1 == null) {
+     preNode.next = l2;
+     } else {
+     preNode.next = l1;
+     }
+
+     return preHead.next;
+     }
+     **/
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
