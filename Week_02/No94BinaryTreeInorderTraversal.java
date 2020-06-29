56b82d063e9521b44f76aa67d4922c8a7420c0b5
Phil Zhang <371418433@qq.com>
2020-06-30 02:40:55 +0800

    Create No94BinaryTreeInorderTraversal.java

---
+++ Week_02/No94BinaryTreeInorderTraversal.java
@@ -0,0 +1,14 @@
+class Solution {
+    private List<Integer> list = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+        inorderTraversal(root.left);
+        list.add(root.val);
+        inorderTraversal(root.right);
+
+        return list;
+    }
+}
